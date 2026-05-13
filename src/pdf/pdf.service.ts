4001dbb52167d4474869ac5203e4279965c944b8
MiguelAdroituser <miguel@adroitoverseas.com>
2026-05-12 17:28:56 -0700

    Feat - update Puppeteer launch configuration for improved performance

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -80,7 +80,15 @@ export class PdfService {
     // const browser = await puppeteer.launch({ headless: true }); // ← cambiado
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      // args: ['--no-sandbox', '--disable-setuid-sandbox']
+      //--deployment configuration
+      executablePath: '/usr/bin/chromium',
+      args: [
+        '--no-sandbox', 
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+      ]
     });
 
     const page = await browser.newPage();
